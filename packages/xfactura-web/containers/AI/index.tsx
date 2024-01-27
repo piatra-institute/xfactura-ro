8283a261b99764a09af57baeacbe2410b21f0daf
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-27 21:34:46 +0200

    xfactura-web > dev: AI

--- packages/xfactura-web/containers/AI/index.tsx
+++ packages/xfactura-web/containers/AI/index.tsx
@@ -10,6 +10,8 @@ export default function AI({
     return (
         <div
             className={`
+                w-full h-full
+                fixed top-0 right-0 bottom-0 z-30
                 grid items-center justify-center place-content-center gap-12
             `}
         >
