357209a21de612084ab1297c0f2da142988540d3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-31 16:56:49 +0200

    xfactura-web > dev: public

--- packages/xfactura-web/app/layout.tsx
+++ packages/xfactura-web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import Head from 'next/head';
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import './globals.css';
@@ -29,6 +31,15 @@ export default function RootLayout({
             lang="ro"
             suppressHydrationWarning
         >
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#2b5797" />
+                <meta name="theme-color" content="#000000" />
+            </Head>
+
             <body className={inter.className}>
                 <GoogleOAuthProvider
                     clientId={ENVIRONMENT.GOOGLE_LOGIN}
