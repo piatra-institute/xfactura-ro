d1a55d4cef63bfccd2010c44884a1fbe42c570fa
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-13 17:44:39 +0200

    xfactura-web > dev: logic/pdf

--- packages/xfactura-web/logic/pdf/index.ts
+++ packages/xfactura-web/logic/pdf/index.ts
@@ -1,5 +1,7 @@
 import {
     PDFDocument,
+    PDFPage,
+    PDFFont,
     rgb,
 } from 'pdf-lib';
 
@@ -16,16 +18,108 @@ import {
 
 
 const drawText = (
-    page: any,
-    text: any,
+    page: PDFPage,
+    text: string,
     x: number,
     y: number,
     size = 10,
-    font: any,
+    font: PDFFont,
     color = rgb(0, 0, 0),
 ) => {
     page.drawText(text, { x, y, size, font, color });
 }
+
+
+const drawSeller = async (
+    data: Invoice,
+    page: PDFPage,
+    font: PDFFont,
+    margin: number,
+    currentY: number,
+) => {
+    drawText(page, `Furnizor: ${data.seller.name}`, margin, currentY, 12, font);
+    currentY -= 20;
+    drawText(page, `${data.seller.address}, ${data.seller.city}, ${data.seller.county}, ${data.seller.country}`, margin, currentY, 10, font);
+    currentY -= 15;
+    if (data.seller.contact) {
+        drawText(page, `Contact: ${data.seller.contact}`, margin, currentY, 10, font);
+        currentY -= 20;
+    } else {
+        currentY -= 5;
+    }
+
+    return currentY;
+}
+
+
+const drawBuyer = async (
+    data: Invoice,
+    page: PDFPage,
+    font: PDFFont,
+    margin: number,
+    currentY: number,
+) => {
+    drawText(page, `Cumpărător: ${data.buyer.name}`, margin, currentY, 12, font);
+    currentY -= 20;
+    drawText(page, `${data.buyer.address}, ${data.buyer.city}, ${data.buyer.county}, ${data.buyer.country}`, margin, currentY, 10, font);
+    currentY -= 30; // Extra space before the invoice lines
+
+    return currentY;
+}
+
+
+const drawInvoiceHeaders = async (
+    data: Invoice,
+    page: PDFPage,
+    font: PDFFont,
+    margin: number,
+    currentY: number,
+) => {
+    const headers = ["Nume", "Cantitate", "Preț", "TVA", "Total"];
+    const columnWidths = [100, 70, 70, 70, 70];
+
+    let currentX = margin;
+    headers.forEach((header, index) => {
+        drawText(page, header, currentX, currentY, 12, font, rgb(0, 0.53, 0.71));
+        currentX += columnWidths[index] + 10; // Adding some space between columns
+    });
+    currentY -= 20;
+
+    return currentY;
+}
+
+
+const drawInvoiceLines = async (
+    data: Invoice,
+    page: PDFPage,
+    font: PDFFont,
+    margin: number,
+    currentY: number,
+) => {
+    const columnWidths = [100, 70, 70, 70, 70];
+
+    data.products.forEach((product) => {
+        const productTotal = product.vatIncluded ? product.price * product.quantity :
+            product.price * product.quantity * (1 + product.vatRate);
+        const line = [
+            product.name,
+            product.quantity.toString(),
+            `$${product.price.toFixed(2)}`,
+            `${(product.vatRate * 100).toFixed(0)}%`,
+            `$${productTotal.toFixed(2)}`
+        ];
+
+        let currentX = margin;
+        line.forEach((text, index) => {
+            drawText(page, text, currentX, currentY, 10, font);
+            currentX += columnWidths[index] + 10;
+        });
+        currentY -= 20;
+    });
+
+    return currentY;
+}
+
 
 export const generatePdf = async (
     data: Invoice,
@@ -39,65 +133,16 @@ export const generatePdf = async (
     const A4 = [595, 842] as [number, number];
     const page = pdfDoc.addPage(A4);
 
-    // Basic layout setup
     const margin = 50;
     const initialY = 700;
     let currentY = initialY;
 
+    currentY = await drawSeller(data, page, font, margin, currentY);
+    currentY = await drawBuyer(data, page, font, margin, currentY);
 
-    // Draw Seller Information
-    drawText(page, `Furnizor: ${data.seller.name}`, margin, currentY, 12, font);
-    currentY -= 20;
-    drawText(page, `${data.seller.address}, ${data.seller.city}, ${data.seller.county}, ${data.seller.country}`, margin, currentY, 10, font);
-    currentY -= 15;
-    if (data.seller.contact) {
-        drawText(page, `Contact: ${data.seller.contact}`, margin, currentY, 10, font);
-        currentY -= 20;
-    } else {
-        currentY -= 5;
-    }
+    currentY = await drawInvoiceHeaders(data, page, font, margin, currentY);
+    currentY = await drawInvoiceLines(data, page, font, margin, currentY);
 
-
-    // Draw Buyer Information
-    drawText(page, `Cumpărător: ${data.buyer.name}`, margin, currentY, 12, font);
-    currentY -= 20;
-    drawText(page, `${data.buyer.address}, ${data.buyer.city}, ${data.buyer.county}, ${data.buyer.country}`, margin, currentY, 10, font);
-    currentY -= 30; // Extra space before the invoice lines
-
-
-    // Draw Invoice Headers
-    const headers = ["Nume", "Cantitate", "Preț", "TVA", "Total"];
-    const columnWidths = [100, 70, 70, 70, 70];
-    let currentX = margin;
-    headers.forEach((header, index) => {
-        drawText(page, header, currentX, currentY, 12, font, rgb(0, 0.53, 0.71));
-        currentX += columnWidths[index] + 10; // Adding some space between columns
-    });
-    currentY -= 20;
-
-
-    // Draw Invoice Lines
-    data.products.forEach((product) => {
-        const productTotal = product.vatIncluded ? product.price * product.quantity :
-            product.price * product.quantity * (1 + product.vatRate);
-        const line = [
-            product.name,
-            product.quantity.toString(),
-            `$${product.price.toFixed(2)}`,
-            `${(product.vatRate * 100).toFixed(0)}%`,
-            `$${productTotal.toFixed(2)}`
-        ];
-
-        currentX = margin;
-        line.forEach((text, index) => {
-            drawText(page, text, currentX, currentY, 10, font);
-            currentX += columnWidths[index] + 10;
-        });
-        currentY -= 20;
-    });
-
-
-    // Generating PDF and triggering download
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
     const filename = `xfactura_${data.metadata.number}.pdf`;
