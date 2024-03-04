8c2d47aff969661cdd0485746dbcde381e0c786b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-03-04 19:32:06 +0200

    xfactura-web > dev: BuyScreen

--- packages/xfactura-web/components/BuyScreen/index.tsx
+++ packages/xfactura-web/components/BuyScreen/index.tsx
@@ -115,7 +115,7 @@ export default function BuyScreen({
     if (clientSecret) {
         return (
             <div
-                className="h-full min-h-[500px] min-w-[420px] overflow-scroll"
+                className="h-full min-h-[500px] md:min-w-[420px] overflow-scroll"
             >
                 <EmbeddedCheckoutProvider
                     stripe={stripePromise}
